471ac4fce1740e4affeab1d0b08b012cbba9c6de
Sami Fiaz <samifiaz@gmail.com>
2021-06-27 23:26:48 +0500

    logging inputs

--- src/action.js
+++ src/action.js
@@ -1,5 +1,15 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
 async function run () {
     console.log('Hello cdn!');
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const npm_build_command = core.getInput('npm_build_command');
+    const github_repo_url = core.getInput('github_repo_url');
+
+
+    console.log(npm_build_command);
+    console.log(github_repo_url);
 }
 
 run();
